6b439a73c2faa651b64c6c5f17bb116397447103
tchebagual71 <tobydieckman@gmail.com>
2024-03-23 02:32:34 -0400

    updates

--- app.py
+++ app.py
@@ -17,9 +17,11 @@ import requests
 from pymongo import MongoClient
 from source import config
 
-MONGODB_URI = config.MONGODB_URI
-BOT_TOKEN = config.BOT_TOKEN
-HELIUS_KEY = config.HELIUS_KEY
+MONGODB_URI = 'mongodb+srv://walletwatchr:walletwatchrpassword@cluster0.ehtddne.mongodb.net/'
+BOT_TOKEN = '6502984988:AAEE65Mor8XNvxhb3irkxtR8WqDHcbdinL8'
+HELIUS_KEY = '9e17b24d-b88d-41e2-86e1-0bb6bf5b8095'
+HELIUS_WEBHOOK_URL = 'https://walletwatchr-cacf9141608c.herokuapp.com/wallet'
+HELIUS_WEBHOOK_ID = '5d8cadd0-ce36-4cb9-8cbc-6dc3d21705f'
 
 client = MongoClient(MONGODB_URI)
 db = client.sol_wallets
--- bot.py
+++ bot.py
@@ -10,13 +10,12 @@ from datetime import datetime
 
 import source.config as config
 from source.bot_tools import *
-from source import config
 
-MONGODB_URI = config.MONGODB_URI
-BOT_TOKEN = config.BOT_TOKEN
-TOKEN = BOT_TOKEN
-HELIUS_KEY = config.HELIUS_KEY
-HELIUS_WEBHOOK_ID = config.HELIUS_WEBHOOK_ID
+MONGODB_URI = 'mongodb+srv://walletwatchr:walletwatchrpassword@cluster0.ehtddne.mongodb.net/'
+BOT_TOKEN = '6502984988:AAEE65Mor8XNvxhb3irkxtR8WqDHcbdinL8'
+HELIUS_KEY = '9e17b24d-b88d-41e2-86e1-0bb6bf5b8095'
+HELIUS_WEBHOOK_URL = 'https://walletwatchr-cacf9141608c.herokuapp.com/wallet'
+HELIUS_WEBHOOK_ID = '5d8cadd0-ce36-4cb9-8cbc-6dc3d21705f'
 
 ADDING_WALLET, DELETING_WALLET = range(2)
 client = MongoClient(MONGODB_URI)
@@ -231,7 +230,7 @@ def show_wallets(update: Update, context: CallbackContext) -> None:
 
 
 def main() -> None:
-    updater = Updater(TOKEN)
+    updater = Updater(BOT_TOKEN)
     dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
