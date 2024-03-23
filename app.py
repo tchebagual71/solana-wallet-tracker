034ace68a483e58874ff2854fd68883b2ef0cd92
tchebagual71 <tobydieckman@gmail.com>
2024-03-23 01:03:14 -0400

    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import requests
 import os
 
 # Your config values
-MONGODB_URI = 'mongodb+srv://test:test@cluster0.ehtddne.mongodb.net/?retryWrites=true&w&majorty&appName=Cluster0'
+MONGODB_URI = 'mongodb+srv://test:test@cluster0.ehtddne.mongodb.net'
 BOT_TOKEN = '6502984988:AAEE65Mor8XNvxhb3irkxtR8WqDHcbdinL8'
 HELIUS_KEY = '9e17b24d-b88d-41e2-86e1-0bb6bf5b8095'
 HELIUS_WEBHOOK_URL = 'https://walletwatchr-cacf9141608c.herokuapp.com/wallet'
