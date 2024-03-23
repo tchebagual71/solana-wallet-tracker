f1b2a08ccfc2d81f448822ad4efed2babd60392b
tchebagual71 <tobydieckman@gmail.com>
2024-03-23 01:21:16 -0400

    asdfds

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ import requests
 import os
 
 # Your config values
-MONGODB_URI = 'mongodb+srv://test:test@cluster0.ehtddne.mongodb.net'
+MONGODB_URI = 'mongodb+srv://walletwatchr:walletwatchrpassword@cluster0.ehtddne.mongodb.net/'
 BOT_TOKEN = '6502984988:AAEE65Mor8XNvxhb3irkxtR8WqDHcbdinL8'
 HELIUS_KEY = '9e17b24d-b88d-41e2-86e1-0bb6bf5b8095'
 HELIUS_WEBHOOK_URL = 'https://walletwatchr-cacf9141608c.herokuapp.com/wallet'
-HELIUS_WEBHOOK_ID = '5d8cadd0-ce36-4cb9-8cbc-6dc3d21705f'
+HELIUS_WEBHOOK_ID = '5d8cadd0-ce36-4cb9-8cbc-6dc3d21705fc'
 
 client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
 db = client.sol_wallets
